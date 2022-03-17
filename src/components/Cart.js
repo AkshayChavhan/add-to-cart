92991ff65a27fe29783428f81e2595f95d89deaa
Akshay_Chavhan <akshaychavhan676@gmail.com>
2022-03-17 14:34:16 +0530

    STAGE 2 DONE ,cartcontext completed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,46 +1,20 @@
-import React ,{useState} from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
+import React ,{createContext} from 'react';
 import './cart.css';
-import Items from './Items';
-import {products} from './products';
+import { products } from './products';
+import ContextCart from './ContextCart';
+
+
+export const CartContext =  createContext() ;  
 
 const Cart = () => {
 
-  const [item , setItem]= useState(products);
+  // const [item , setItem]= useState(products);
 
-  return (
+  return ( 
     <>
-    <header>
-      <div className="continue-shopping">
-          <img src="./images/arrow.png" alt="arrow" className='arrow-icon'></img>
-          <h3>Continue shopping</h3>
-      </div>
-      <div className='cart-icon'>
-        <img src='./images/cart.png' alt='cart'></img>
-        <p>7</p>
-      </div>
-    </header>
-
-    <section className='main-cart-section'>
-      <h1>Shopping Cart</h1>
-      <p>You have <span className='total-items-count'>7</span> items in shopping cart</p>
-
-      <div className='cart-items'>
-        <div className='cart-items-container'>
-          <Scrollbars >
-          {
-            item.map((curItem) => {
-                  return <Items key={curItem.id} {...curItem} />
-            })
-          }
-          </Scrollbars> 
-        </div>
-      </div>
-      <div className='card-total'>
-        <h3>Card Total : <span> 2200rs</span></h3>
-        <button>Checkout :</button>
-      </div>
-    </section>
+    <CartContext.Provider value={products}>
+      <ContextCart />
+    </ CartContext.Provider > 
     </>
   )
 }
